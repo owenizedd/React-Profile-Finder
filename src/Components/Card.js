189b1ee36f9bf79e3f7814debd01c7371cb86764
Ryan Owen Thionanda <owenizedd@gmail.com>
2021-11-21 11:15:28 +0700

    fix again

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,7 +4,7 @@ import "./Card.style.css";
 export default ({ photo_url, name, email }) => {
   return (
     <div className="card">
-      <img src={photo_url} alt='card photo'/>
+      <img src={photo_url} alt='card-img'/>
       <br />
       <h3>{name}</h3>
       <h4>{email}</h4>
